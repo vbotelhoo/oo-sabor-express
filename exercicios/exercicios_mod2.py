f8cf10ca28e860e8d5a03e8267610fd297da25d6
vbotelhoo <vt.botelhoo@gmail.com>
2024-11-02 09:13:15 -0300

    exercicios e implementacao de metodo de classe

--- exercicios/exercicios_mod2.py
+++ exercicios/exercicios_mod2.py
@@ -38,4 +38,4 @@ class Cliente:
 # Instanciando três objetos da classe Cliente e atribuindo valores aos seus atributos através do construtor
 cliente1 = Cliente(nome='Alice', idade=25, email='alice@gmail.com', telefone='123-456-7890')
 cliente2 = Cliente(nome='Bob', idade=30, email='bob@gmail.com', telefone='987-654-3210')
-cliente3 = Cliente(nome='Charlie', idade=22, email='charlie@gmail.com', telefone='555-123-4567')
+cliente3 = Cliente(nome='Charlie', idade=22, email='charlie@gmail.com', telefone='555-123-4567')    
--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,19 +1,31 @@
 class Restaurante:
     restaurantes = []
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return self.nome
+        return self._nome
     
-    def listar_restaurantes():
-        for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
-        
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'.ljust(25)}')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo.ljust(25)}')
+
+    @property
+    def ativo(self):
+        return '☑' if self._ativo else '☐'
+    
+    def alternar_status(self):
+        self._ativo = not self._ativo
+
+
+
 restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_praca.alternar_status()
 restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
 Restaurante.listar_restaurantes()
