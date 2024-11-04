d24b696b656b61787d3731b35957f5f56b27138e
vbotelhoo <vt.botelhoo@gmail.com>
2024-11-04 18:26:00 -0300

    herança, polimorfismo e vapo

--- app.py
+++ app.py
@@ -4,11 +4,14 @@ from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
 bebida_suco = Bebida('Suco de Melancia', 5.00, 'Grande')
+bebida_suco.aplicar_desconto
 prato_paozinho = Prato('Paozinho', 2.00, 'O melhor pão da cidade')
+prato_paozinho.aplicar_desconto
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
 
 def main():
-        print(bebida_suco)
-        print(prato_paozinho)
+        restaurante_praca.exibir_cardapio
 
 if __name__ == '__main__':
     main()
